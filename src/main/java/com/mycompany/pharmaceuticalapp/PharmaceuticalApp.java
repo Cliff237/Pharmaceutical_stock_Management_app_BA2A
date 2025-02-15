9c621e1817a60718d04a03bc8e6578a7e917c203
Cliff237 <momocliff2004@gmail.com>
2025-02-15 12:43:30 +0000

    Add files via upload
    Creation of dashboard with their functionalities done

---
+++ src/main/java/com/mycompany/pharmaceuticalapp/PharmaceuticalApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pharmaceuticalapp;
+
+/**
+ *
+ * @author cliffm
+ */
+import Viewas.LogInPage;
+public class PharmaceuticalApp {
+
+    public static void main(String[] args) {
+        LogInPage log = new LogInPage();
+        log.setVisible(true);
+    }
+}
